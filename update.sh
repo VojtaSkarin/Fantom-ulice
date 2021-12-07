0c04f2dbab9f250a3390f9e3503bc0cf09020e5b
Vojtěch Škaroupka <xskarou1@fi.muni.cz>
2021-12-07 19:09:11 +0100

    Oprava překlepu v update.sh

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ git restore *
 git pull
 
 for FILE in content/*.php index.php; do
-	if [ "$FILE" != "content/pre.php" ] && [ "$FILE" != "content/post.php" ] && [ "$FILE" != "content/intro.header.php" ]; then
+	if [ "$FILE" != "content/pre.php" ] && [ "$FILE" != "content/post.php" ] && [ "$FILE" != "content/intro-header.php" ]; then
 		sed -i '1 i\
 #!/packages/run/php/bin/php' $FILE
 		chmod u+x $FILE
